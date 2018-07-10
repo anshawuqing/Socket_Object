ec0a296cc53f78b4ed620a6d38f08b922bbc8122
杨万盛 <1748896856@qq.com>
2018-07-10 18:32:22 +0800

    客户端代码 完成

---
+++ Assets/scrip/ClientSocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace  Net
+{
+
+    /// <summary>
+    /// Socket 客户端 通信 代码 
+    /// </summary>
+     public class ClientSocket
+     {
+
+	// Use this for initialization
+	void Start ()
+        {
+		
+	    }
+	
+	// Update is called once per frame
+	void Update ()
+        {
+		
+	    }
+     }
+}
